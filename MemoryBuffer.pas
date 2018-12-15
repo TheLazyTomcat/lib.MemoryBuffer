44a36cdd79a50b802cd5ff126a7d12eee6148019
František Milt <frantisek.milt@gmail.com>
2018-12-15 14:07:39 +0100

    Added function CopyBufferInto, suppressed out-of-place warnings in Lazarus/FPC

--- MemoryBuffer.pas
+++ MemoryBuffer.pas
@@ -21,6 +21,8 @@ unit MemoryBuffer;
 
 {$IFDEF FPC}
   {$MODE ObjFPC}{$H+}
+  {$DEFINE FPC_DisableWarns}
+  {$MACRO ON}
 {$ENDIF}
 
 interface
@@ -52,15 +54,23 @@ procedure ReallocBuffer(var Buff: TMemoryBuffer; NewSize: TMemSize);
 procedure ReallocBufferKeep(var Buff: TMemoryBuffer; NewSize: TMemSize; AllowShrink: Boolean = False);
 
 procedure CopyBuffer(const Src: TMemoryBuffer; out Copy: TMemoryBuffer);
+procedure CopyBufferInto(const Src: TMemoryBuffer; var Dest: TMemoryBuffer);
 
 Function BuildBuffer(Memory: Pointer; Size: TMemSize; Data: PtrInt = 0): TMemoryBuffer;
 
 implementation
 
+{$IFDEF FPC_DisableWarns}
+  {$DEFINE FPCDWM}
+  {$DEFINE W5058:={$WARN 5058 OFF}} // Variable "$1" does not seem to be initialized
+{$ENDIF}
+
+{$IFDEF FPCDWM}{$PUSH}W5058{$ENDIF}
 procedure InitBuffer(out Buff: TMemoryBuffer);
 begin
 FillChar(Buff,SizeOf(TMemoryBuffer),0);
 end;
+{$IFDEF FPCDWM}{$POP}{$ENDIF}
 
 //------------------------------------------------------------------------------
 
@@ -155,6 +165,15 @@ begin
 Copy := GetBuffer(Src.Size);
 Move(Src.Memory^,Copy.Memory^,Src.Size);
 Copy.Data := Src.Data;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure CopyBufferInto(const Src: TMemoryBuffer; var Dest: TMemoryBuffer);
+begin
+// destination buffer must be properly initialized
+ReallocBuffer(Dest,Src.Size);
+Move(Src.Memory^,Dest.Memory^,Dest.Size);
 end;
 
 //------------------------------------------------------------------------------
